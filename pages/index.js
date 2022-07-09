bf6faf8724a331b6b9d50f4b85907d563c5cd051
Mitch McCullough <mitchmccullough@gmail.com>
2022-07-09 14:00:51 -0500

    added buttons

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
+import { useState } from 'react';
+
 function Home() {
+  // useState returns an array, the first thing being the value (taco; anytime we wanna update value we use setValue (taco, like setTaco)
+  // useState is a hook. We need to pass it a default
+  const [value, setValue] = useState(0);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home! </h1>
-    </div>
+    <>
+      <h5>Who wants to Free PawPaw?</h5>
+      <h2>{ value }</h2>
+      {/* think of as asyncronous call like state promise ------------------> OLD WAY BELOW */}
+      {/* <button type="button" onClick={() => setValue(value + 1)}>Free Him!</button>
+      <button type="button" onClick={() => setValue(value - 1)}>Don&apos;t Free Him! I hate him!</button>
+      <button type="button" onClick={() => setValue(0)}>Reset(I dont care)</button> */}
+      {/* wait until value updates, then adds one when it does -------------------> BETTER WAY BELOW */}
+      <button type="button" onClick={() => setValue((prevState) => prevState + 1)}>Free Him!</button>
+      <button type="button" onClick={() => setValue((prevState) => prevState - 1)}>Don&apos;t Free Him! I hate him!</button>
+      <button type="button" onClick={() => setValue(0)}>Reset(I dont care)</button>
+    </>
   );
 }
 
--- pages/pawpaw.js
+++ pages/pawpaw.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Loading from '../components/Loading';
 
 export default function pawpaw() {
+  // showing component Loading on DOM with JSX syntax; the <> </> is a FRAGMENT, like an empty DIV
   return (
-    <div>pawpaw</div>
+    <>
+      <Loading />
+      <div>Hello, PawPaw</div>
+      <div>
+        <h3>Should we Free PawPaw from Jail?</h3>
+        <button type="submit">Like this if You Want to Free PawPaw from Jail</button>
+        <button type="submit">Like this if You Don&apos;t Want to Free PawPaw from Jail</button>
+        <button type="submit">Like this to Reset</button>
+      </div>
+    </>
   );
+  // return ( <--------------------- OLD WAY
+  //   Loading()
+  // );
 }
